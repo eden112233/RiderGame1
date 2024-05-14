71abe5594d52ad6e227994f320595e4c85d9d4e4
edi <edibersh@gmail.com>
2024-05-14 11:23:28 -0500

    updated jumping. car is moving withought touch

--- app/src/main/java/com/example/ridergame/Car.java
+++ app/src/main/java/com/example/ridergame/Car.java
@@ -6,37 +6,33 @@ import android.graphics.Canvas;
 class Car {
     private float x;
     private float y;
-    private  float m1;
+    private float m1;
+    private float jumpHeight;
+    private boolean isJumping = false;
     Bitmap bitmapCar;
     boolean carJump;
     Point p;
-
 
     public Car(float x, float y, Bitmap bitmap) {
         this.x = x;
         this.y = y;
         this.bitmapCar = bitmap;
+        this.jumpHeight = y; // Initialize jumpHeight to the original y position
     }
 
-
-    public void setM1(float m1)
-    {
+    public void setM1(float m1) {
         this.m1 = m1;
     }
 
-    public void setPosition(Point pPosition)
-    {
-         this.x = pPosition.getX();
-        this.y=pPosition.getY()- bitmapCar.getHeight();
-        if(carJump) //לעשןת counter שאחרי 10 פעמים שזה מוריד אותו ב20 זה עוצר כדי שזה יגיע ל200 שזה כמה שהעלנו
-        {
+    public void setPosition(Point pPosition) {
+        this.x = pPosition.getX();
+        this.y = pPosition.getY() - bitmapCar.getHeight();
+        if (carJump) {
             y = y + 20;
         }
-
     }
 
-    public void draw(Canvas canvas)
-    {
+    public void draw(Canvas canvas) {
         float bitmapCenterX = (this.x + (bitmapCar.getWidth() / 2));
         float bitmapCenterY = this.y + (bitmapCar.getHeight() / 2);
         canvas.save();
@@ -45,14 +41,27 @@ class Car {
         canvas.restore();
     }
 
-    public int getCarHeight ()
-    {
-        int h= bitmapCar.getHeight();
-        return h;
+    public int getCarHeight() {
+        return bitmapCar.getHeight();
     }
 
     public void jump() {
-        carJump = true;
-        y = y -200;
+        if (!isJumping) {
+            carJump = true;
+            isJumping = true;
+            jumpHeight = y;
+            y = y - 200;
+        }
+    }
+
+    public void update() {
+        if (isJumping) {
+            y += 50; // Adjust this value to control the speed of descent
+            if (y >= jumpHeight) {
+                y = jumpHeight;
+                carJump = false;
+                isJumping = false;
+            }
+        }
     }
 }
